98384e7e0ad96c8a86b46f06e81437941b034c8f
AyshenDjamal <ayshen.rasul92@gmail.com>
2025-03-07 18:51:50 +0400

    created custom class loader app

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        MyClassLoader myClassLoader = new MyClassLoader();
+        Class<?> messagePrinter = myClassLoader.load("MessagePrinter");
+
+
+            Object object = messagePrinter.getDeclaredConstructor().newInstance();
+            Method printMessage = messagePrinter.getMethod("printMessage");
+
+            printMessage.invoke(object);
+
+    }
+}
---
+++ src/MessagePrinter.java
@@ -0,0 +1,6 @@
+public class MessagePrinter {
+
+    public void printMessage (){
+        System.out.println("hello world");
+    }
+}
